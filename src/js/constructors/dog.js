0a252a8d12086165dad2549552b28c82db84be6b
thomasweld <thomas.weld@gmail.com>
2016-05-03 23:22:58 -0400

    passing tests

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,2 +1,17 @@
-let Dog = function () {}
+let Dog = function ( options ) {
+
+  options = options || {};
+
+  this.status = 'normal';
+  this.color = options.color || 'undefined';
+
+  if (options.hungry === undefined){
+      this.hungry = true;
+  } else {
+    this.hungry = options.hungry;
+  };
+
+}
+
+
 export default Dog;
--- src/js/constructors/human.js
+++ src/js/constructors/human.js
@@ -1,2 +1,22 @@
-let Human = function () {}
+let Human = function ( options ) {
+
+  options = options || {};
+
+  this.pet = function ( dog ){
+    dog = dog || {};
+    dog.status = 'happy';
+  };
+
+  this.feed = function ( dog ){
+    dog.hungry = false;
+  };
+
+  if (options.cool === undefined){
+    this.cool = false;
+  } else {
+    this.cool = options.cool;
+  }
+}
+
+
 export default Human;
